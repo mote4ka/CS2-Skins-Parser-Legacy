862fdf3ada13a2dad5eea519523c82d71836fafb
Матвей <tochnonebolotnyaytka@gmail.com>
2025-08-25 20:30:04 +0300

    final version

--- dataworker.py
+++ dataworker.py
@@ -6,7 +6,7 @@ import requests
 
 import sys
 
-#from data_full import *
+from config import *
 
 
 
@@ -32,12 +32,13 @@ def csm_data_process(output_file):
         if any(ignore_word in name for ignore_word in ignorelist):
             skipped_counter +=1
         else:
-            # add item in dict with volume and price
-            result_dict[name] = {'price' : item.get('price'),
-                             'volume': item.get('volume')}
-       
-            # increment counter
-            counter += 1
+            if float(item.get('price')) >= low_filter and float(item.get('price')) <= high_filter:
+                # add item in dict with volume and price
+                result_dict[name] = {'price' : item.get('price'),
+                                'volume': item.get('volume')}
+        
+                # increment counter
+                counter += 1
 
     # save dict to file
     with open(output_file, 'w', encoding='utf-8') as file:
@@ -46,69 +47,184 @@ def csm_data_process(output_file):
     # print items amount
     print(f"CSM saved {counter} items, skipped {skipped_counter} items")
 
+def lsk_get_data(output_file):
+    with open(output_file, "w", encoding="utf-8") as file:
+        req = requests.get("https://lis-skins.com/market_export_json/api_csgo_full.json")
+        file.write(str(req.content))
+    print("lsk data collected!")
 
+def lsk_process_data(input_file, output_file):
 
-def lsk_collect_data(output_file):
-    f = io.open(output_file, 'w',encoding="utf-8")
-
-    req = requests.get("https://lis-skins.com/market_export_json/api_csgo_full.json")
+    with open(input_file, 'r', encoding='utf-8', errors='ignore') as file:
+        content = file.read().strip()
     
-    f.write(str(req.content))
-    f.close()
-    print("Lsk data collected!")
-
-
-
-def lsk_data_process(input_file, output_file):
+    objects = re.findall(r'\{[^{}]*\}', content)
     
-    # open input file with raw data
-    with open(input_file, 'r', encoding='utf-8') as file:
-        content = file.read()
+    unique_objects = {}
+    duplicate_count = 0
     
-    # seek for all names and price in file
-    name_pattern = r'"name"\s*:\s*"([^"]*)"'
-    price_pattern = r'"price"\s*:\s*([0-9.]+)'
-    
-    names = re.findall(name_pattern, content)
-    prices = re.findall(price_pattern, content)
-    
-    # set price to equal name (same oreder)
-    result_dict = {}
-    min_len = min(len(names), len(prices))
-
-    # get names to ignore
-    with open('ignorelist.txt', 'r',encoding='utf-8') as file:
-        ignorelist = file.read().splitlines()
-    skipped_counter = 0
-    
-
-    for i in range(min_len):
-        # get name and fix stattrack and knifes symbols
-        name = names[i].replace('\\\\u2122','™').replace('\\\\u2605','★')
-        price = prices[i]
+    for obj in objects:
+        name_match = re.search(r'"name"\s*:\s*"([^"]*)"', obj)
         
-        # check if item in ignore list
-        if any(ignore_word in name for ignore_word in ignorelist):
-            skipped_counter +=1
+        if name_match:
+            key = f"{name_match.group(1)}"
+            
+            if key not in unique_objects:
+                unique_objects[key] = obj
+            else:
+                duplicate_count += 1
         else:
-            if name not in result_dict:
-                try:
-                    result_dict[name] = float(price)
-                except ValueError:
-                    result_dict[name] = price
+            if obj not in unique_objects:
+                unique_objects[obj] = obj
+            else:
+                duplicate_count += 1
     
+    items_start = content.find('"items":[') + 8
+    items_end = content.rfind(']')
+    
+    if items_start > 0 and items_end > items_start:
+        before_items = content[:items_start]
+        after_items = content[items_end:]
+        new_content = before_items + ','.join(unique_objects.values()) + after_items
+    else:
+        new_content = '{"status":"success","items":[' + ','.join(unique_objects.values()) + ']}'
+    
+    with open(output_file, 'w', encoding='utf-8') as file:
+        file.write(new_content)
+    
+    print(f"Deleted {duplicate_count} repeating items")
+    print(f"Saved {len(unique_objects)} unique items")
+
+
+
+
+    with open(output_file, "r", encoding="utf-8") as file:
+        content = file.read()
+
+    if content.startswith("b'") or content.startswith('b"'):
+        content = content[2:-1]
+    
+    content = re.sub(r'"name":"([^"]*)\"}\]\},', r'"name":"\1"},', content)
+
+    content = re.sub(r'"name":"([^"]*)\"}\],', r'"name":"\1"},', content)
+    content = re.sub(r'"name":"([^"]*)\"}\]', r'"name":"\1"}', content)
+
+    content = re.sub(r"\\'", "'", content)
+    fields_to_remove = [
+        
+        'id',
+        'stickers',
+        'item_paint_seed',
+        'item_paint_index', 
+        'item_float',
+        'unlock_at',
+        'created_at',
+        'game_id',
+        'name_tag',
+        'item_class_id',
+        'item_asset_id',
+        'image',
+        'wear',
+        'slot'
+    ]
+    
+    for field in fields_to_remove:
+        patterns = [
+            f'"{field}"\\s*:\\s*[^,}}]*,?',  # простые значения
+            f'"{field}"\\s*:\\s*"[^"]*",?',   # строковые значения
+            f'"{field}"\\s*:\\s*\\d+,?',      # числовые значения
+            f'"{field}"\\s*:\\s*null,?',      # null значения
+            f'"{field}"\\s*:\\s*\\[.*?\\],?'  # массивы
+        ]
+        
+        for pattern in patterns:
+            content = re.sub(pattern, '', content, flags=re.DOTALL | re.IGNORECASE)
+    
+    # cleaning json file
+    content = re.sub(r'\"\]\}(\s*,)?', '"}\1', content)  # "]} -> "}
+    content = re.sub(r'\"\}\](\s*,)?', '"}\1', content)  # "}] -> "}
+
+    content = re.sub(r',\s*}', '}', content)
+    content = re.sub(r',\s*]', ']', content)
+
+    content = re.sub(r'(\"items\"\s*:\s*\[)[^]]*$', r'\1]}', content)
+
+    p = re.compile('(?<!\\\\)\'')
+    content = p.sub('\"', content)
+
+    # save
+    with open(output_file, 'w', encoding='utf-8') as file:
+        file.write(content)
+
+
+    # fix json file
+    try:
+        objects = re.findall(r'\{[^{}]*\}', content)
+        items = []
+        
+        for obj in objects:
+            try:
+                item_data = json.loads(obj)
+                if 'name' in item_data and 'price' in item_data:
+                    items.append({"name": item_data['name'].replace("\\u2122", "™").replace("\\u2605","★"), "price": item_data['price']})
+            except:
+                continue
+        
+        new_json = {"status": "success", "items": items}
+        
+        with open(output_file, 'w', encoding='utf-8') as file:
+            json.dump(new_json, file, ensure_ascii=False, indent=2)
+        return True
+        
+    except Exception as e:
+        print(f"JSON file broken: {e}")
+
+    print("lsk data processed!")
+
+
+
+def lsk_data_parse(input_file, output_file):
+    
+    result_dict = {}
+
+    with open(input_file, 'r', encoding='utf-8', errors='ignore') as file:
+        items = json.loads(file.read()).get('items')
+        
+        for item in items:
+            try:
+                if isinstance(item, dict) and 'name' in item and 'price' in item:
+                    name = str(item['name'])
+                    price = item['price']
+                    
+                    if name not in result_dict:
+                        result_dict[name] = price
+                    else:
+                        print("skip")
+            except:
+                continue
+
+
     # save the result in output file
-    import json
     with open(output_file, 'w', encoding='utf-8') as file:
         json.dump(result_dict, file, ensure_ascii=False, indent=2)
     
     # print items amount
-    print(f"Lsk saved {len(result_dict)} items, skipped {skipped_counter} items")
+    print(f"Lsk saved {len(result_dict)} items")
 
 # args handler
 if 'csm' in sys.argv:
     csm_data_process('temp/csm_data.json')
-if 'lskdata' in sys.argv:
-    lsk_collect_data('temp/lsk_rawdata.json')
+if 'lskgetdata' in sys.argv:
+    lsk_get_data('temp/lsk_rawdata.json')
+if 'lskprocessdata' in sys.argv:
+    lsk_process_data('temp/lsk_rawdata.json','temp/lsk_processed.json')
+if 'lskparse' in sys.argv:
+    lsk_data_parse('temp/lsk_processed.json', 'temp/lsk_data.json')
 if 'lsk' in sys.argv:
-    lsk_data_process('temp/lsk_rawdata.json', 'temp/lsk_data.json')
+    lsk_process_data('temp/lsk_rawdata.json','temp/lsk_processed.json')
+    lsk_data_parse('temp/lsk_processed.json', 'temp/lsk_data.json')
+if '-full' in sys.argv:
+    csm_data_process('temp/csm_data.json')
+    lsk_get_data('temp/lsk_rawdata.json')
+    lsk_process_data('temp/lsk_rawdata.json','temp/lsk_processed.json')
+    lsk_data_parse('temp/lsk_processed.json', 'temp/lsk_data.json')
--- parser.py
+++ parser.py
@@ -5,66 +5,18 @@ import cloudscraper
 import time
 import io
 import random
+import sys
 
 import xlrd
 import xlwt
 from xlutils.copy import copy
 
-from data_min import *
+from data import *
 
 from config import *
 
-def find_price(i , item):
-    print(i, end = ' ')
-    #geneation hashnames
-    csm_hashname = item.replace('|', "%7C").replace("(", "%28").replace(")", "%29").replace(" ", "%20")
-    lsk_hashname = item.lower().replace(' | ', "-").replace(" (", "-").replace(")", "").replace(" ", "-").replace("'", '%27')
-    ws.write(i+1, 0, item)
-
-    #request
-    csm_url = "https://market.csgo.com/ru/" + csm_hashname
-    lsk_url = "https://lis-skins.com/market/csgo/" + lsk_hashname
-
-    #lsk scrapping
-    scraper = cloudscraper.create_scraper(browser={'browser': 'firefox','platform': 'windows','mobile': False})
-    req = scraper.get(lsk_url)
-    print(req.status_code)
-
-    attempts = 1
-    while req.status_code != 200 and attempts <= 5:
-        scraper = cloudscraper.create_scraper(browser={'browser': 'firefox','platform': 'windows','mobile': False})
-        req = scraper.get(lsk_url)
-        print("Attempt ", attempts)
-        attempts += 1
-        time.sleep(1)
-    if req.status_code == 200:
-        print(item,end=' ')
-        soup = BeautifulSoup(req.content, 'html.parser')
-        price = soup.find('div', class_='min-price-value').get_text(separator='|',strip=True)
-        lsk_price = price[:price.find('|')].replace(' ', '')
-        ws.write(i+1, 1, float(lsk_price))
-        print(lsk_price,end=' ')
-
-        #csm scrapping
-        csm_price = data.get(item).get('price')
-        csm_volume = data.get(item).get('volume')
-        print(csm_price, csm_volume)
-        profit = float(csm_price)*0.95 - float(lsk_price)*1.05
-        ws.write(i+1,2, csm_price)
-        ws.write(i+1,3, profit)
-        ws.write(i+1,4, profit/float(lsk_price)*1.05*100)
-        ws.write(i+1,5, csm_volume)
-
-    elif req.status_code == 429:
-        print('Too much request, changing proxy..')
-        #proxy = getproxy()
-        raise Exception('429 Too much request')
-    else:
-        print(req.status_code)
-        print('Skip ' + item)
-    wb.save('output.xls')
-    time.sleep(0.2)
-
+if '-full' in sys.argv:
+    from dataworker import *
 
 rb = xlrd.open_workbook('output.xls')
 
@@ -79,34 +31,44 @@ ws.write(0,3, "Profit")
 ws.write(0,4, "%")
 ws.write(0,5, "Volume")
 
+xlwt.add_palette_colour("custom_link", 0x21) 
+wb.set_colour_RGB(0x21, 155, 194, 230) 
+style_bg_link = xlwt.easyxf('pattern: pattern solid, fore_colour custom_link')
+
+lsk_data = get_lsk_data()
+csm_data = get_csm_data()
+
+i=0
+for item in csm_data:
+    
+
+    if lsk_data.get(item) != None:
+        lsk_price = float(lsk_data.get(item))*usdrub
+
+        csm_price = csm_data.get(item).get('price')
+        csm_volume = csm_data.get(item).get('volume')
+
+        profit = float(csm_price)*0.95 - float(lsk_price)*1.05
+
+        #print(item, lsk_price, csm_price, profit)
+
+        csm_hashname = item.replace('|', "%7C").replace("(", "%28").replace(")", "%29").replace(" ", "%20")
+        lsk_hashname = item.lower().replace(' | ', "-").replace(" (", "-").replace(")", "").replace(" ", "-").replace("'", '%27')
+
+        lsk_url = f"https://lis-skins.com/market/csgo/{lsk_hashname}"
+        csm_url = f"https://market.csgo.com/en/{csm_hashname}"
+
+        ws.write(i+1,0, item)
+        ws.write(i+1,1, xlwt.Formula(f'HYPERLINK("{lsk_url}", "{float(lsk_price)}")'), style_bg_link)
+        ws.write(i+1,2, xlwt.Formula(f'HYPERLINK("{csm_url}", "{float(csm_price)}")'),style_bg_link)
+        ws.write(i+1,3, profit)
+        ws.write(i+1,4, profit/float(lsk_price)*1.05*100)
+        ws.write(i+1,5, float(csm_volume))
+
+        i+=1
+
+
 wb.save('output.xls')
 
-indexf = open('index.txt', 'w')
-
-#rotator = pr(proxy_list=PROXIES, change_every = 250)
-
-for i, item in enumerate(data):
-    try:
-        #print(i, item)
-        if 'AWP' in item:
-            find_price(i, item)
-        else:
-            wb.save('output.xls')
-            exit()
-    except KeyboardInterrupt as e:
-        print("Closing...")
-        indexf.write(str(i))
-        wb.save('output.xls')
-        exit()
-
-    except BaseException as e:
-        print(e)
-        print("Restarting...")
-        
-        #find_price(i, data[i])
-
-        indexf.write(str(i))
-        wb.save('output.xls')
 
 wb.save('output.xls')
-indexf.close()
